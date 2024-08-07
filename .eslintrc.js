e47e9e5b90032d02df334c044ed856bcb27c1476
Ricardo Ramirez <ricardo@MacBook-Pro-de-Ricardo.local>
2024-08-07 01:47:34 -0300

    error de compilacion

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   plugins: ['perfectionist', 'unused-imports', '@typescript-eslint', 'prettier'],
-  extends: ['airbnb', 'airbnb-typescript', 'airbnb/hooks', 'prettier', 'plugin:storybook/recommended'],
+  extends: ['airbnb', 'airbnb-typescript', 'airbnb/hooks', 'prettier'],
   parserOptions: {
     sourceType: 'module',
     ecmaVersion: 'latest',
